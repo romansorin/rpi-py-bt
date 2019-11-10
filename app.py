6dbae9644cc9d885ca2a16b67e9791ce50b5650f
Roman Sorin <roman@romansorin.com>
2019-11-09 22:52:47 -0500

    gitignore

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import requests
 from selenium import webdriver
 
 app = Flask(__name__)
@@ -7,6 +6,7 @@ app.config["DEBUG"] = True
 
 DRILL = 'drill'
 BASS = 'bass'
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -17,10 +17,12 @@ def index():
 def start_sounds():
     chrome = webdriver.Chrome()
     sound_type = request.form['sound_type']
+
     if sound_type == DRILL:
         chrome.get('https://www.youtube.com/watch?v=fc7XA4bo4bc&t=2683s')
     elif sound_type == BASS:
         chrome.get('https://www.youtube.com/watch?v=H8pZmH5DHnQ&t=408s')
+
     chrome.find_element_by_class_name('ytp-play-button').click()
     return 'Started.'
 
